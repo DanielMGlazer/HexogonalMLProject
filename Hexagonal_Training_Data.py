747820d6e903608bb9774e4bf1ae930a0c5dbd9e
DanielMGlazer <66266159+DanielMGlazer@users.noreply.github.com>
2020-06-08 13:44:46 -0500

    Implemented central atom. Bug in x-y

--- Hexagonal_Training_Data.py
+++ Hexagonal_Training_Data.py
@@ -11,8 +11,6 @@ from scipy import ndimage
 #%%
 #Defining functions of hexogonal grid
 Point = collections.namedtuple("Point", ["x", "y"])
-
-
 _Hex = collections.namedtuple("Hex", ["q", "r", "s"])
 
 def Hex(q, r, s): #creates the hexagon datatype 
@@ -61,12 +59,9 @@ def hex_neighbor(hex, direction):
 
 # Datatypes for pointy top or flattop lattices
 Orientation = collections.namedtuple("Orientation", ["f0", "f1", "f2", "f3", "b0", "b1", "b2", "b3", "start_angle"])
-
 Layout = collections.namedtuple("Layout", ["orientation", "size", "origin"])
-
-layout_pointy = Orientation(math.sqrt(3.0), math.sqrt(3.0) / 2.0, 0.0, 3.0 / 2.0, math.sqrt(3.0) / 3.0, -1.0 / 3.0, 0.0, 2.0 / 3.0, 0.5)
-layout_flat = Orientation(3.0 / 2.0, 0.0, math.sqrt(3.0) / 2.0, math.sqrt(3.0), 2.0 / 3.0, 0.0, -1.0 / 3.0, math.sqrt(3.0) / 3.0, 0.0)
-
+layout_flat = Orientation(math.sqrt(3.0), math.sqrt(3.0) / 2.0, 0.0, 3.0 / 2.0, math.sqrt(3.0) / 3.0, -1.0 / 3.0, 0.0, 2.0 / 3.0, 0.5)
+layout_pointy = Orientation(3.0 / 2.0, 0.0, math.sqrt(3.0) / 2.0, math.sqrt(3.0), 2.0 / 3.0, 0.0, -1.0 / 3.0, math.sqrt(3.0) / 3.0, 0.0)
 
 #Functions for locating points in xy coordinates
 def hex_to_pixel(layout, h): #returns the x,y coordinate of the center of the hexagon
@@ -74,7 +69,6 @@ def hex_to_pixel(layout, h): #returns the x,y coordinate of the center of the he
     x = (M.f0*h.q+M.f1*h.r)*layout.size.x
     y = (M.f2*h.q+M.f3*h.r)*layout.size.y
     return Point(x + layout.origin.x , y + layout.origin.y)
-
 
 def hex_corner_offset(layout, corner): #Returns how far off each corner is from the center point
     M = layout.orientation
@@ -85,7 +79,7 @@ def hex_corner_offset(layout, corner): #Returns how far off each corner is from 
 def polygon_corners(layout, h): #Creates an array of corners by applying the corner offset method to the center six times
     corners = []
     center = hex_to_pixel(layout, h)
-    for i in range(0, 6):
+    for i in range(4, 6):
         offset = hex_corner_offset(layout, i)
         corners.append(Point(center.x + offset.x, center.y + offset.y))
     return corners
@@ -106,9 +100,9 @@ def to_array(corners): # turns corners into an array of arrays for coordinates
 # Creates rectangular map of hexagons for flat top, origin is in top left
 def rect_map(map_height,map_width):
     map=[]
-    for q in range(map_height):
-        q_offset=math.floor(q/2)
-        for r in range(-q_offset,map_width-q_offset):
+    for r in range(map_height):
+        r_offset=math.floor(r/2)
+        for q in range(-r_offset,map_width-r_offset):
             #map.append(Hex(q,r-map_height//2,-(q)-(r-map_height//2)))
             map.append(Hex(q,r,-q-r))
     return map
@@ -137,18 +131,16 @@ def draw_circ(p,radius,color,draw):
     p2=(p[0]+radius,p[1]+radius)
     draw.ellipse([p1,p2],outline=color)
 
-
-def Gauss_circ(p,radius,color,draw):
+def gauss_circ(p,radius,color,draw):
     _scale=0.5*radius
     for i in range(0,2*radius):
         color_scaling=math.sqrt(2*3.1415*_scale)*240*norm.pdf(i,scale=_scale)
         color_circ=f"hsv({max(color_scaling,.01)},100%,50%)"
         draw_circ(p,i,color_circ,draw)
 
-
 #Function for drawing gaussian dots pixel by pixel
 Gaussian= lambda x,scl,amp: amp*math.e**(-0.5*(x/scl)**2)
-def Gauss_circ_pixel(p,radius):
+def gauss_circ_pixel(p,radius):
     p[0]=int(p[0])
     p[1]=int(p[1])
     #radius is a given param that represents the standard deviation of the gaussian
@@ -174,19 +166,17 @@ def gauss_to_color():
             pixel_array[i][j][1]=rgbcolor[1]
             pixel_array[i][j][2]=rgbcolor[2]
 
-
-
 def plot_hex_dots(h,radius,color,draw,_type):
     assert _type=="Circle" or _type=="Gaussian" or _type=="Gaussian_pixel"
     corners=to_array(polygon_corners(layout,h))
     #corners.append(hex_to_pixel(layout,h)) #Drawing center dot as well. Not needed 
-    for p in corners[:2]:
+    for p in corners:
         if _type == 'Circle':
             solid_circ(p,radius,color,draw)
         if _type == "Gaussian":
-            Gauss_circ(p,radius,color,draw)
+            gauss_circ(p,radius,color,draw)
         if _type=="Gaussian_pixel":
-            Gauss_circ_pixel(p,radius)
+            gauss_circ_pixel(p,radius)
 
 #%%
 #Noise creation functions
@@ -200,38 +190,113 @@ def make_noisy_image(image,l):
     image_n_filt = np.random.poisson(image* vals) / float(vals)
     return image_n_filt
 
+#%%
+#Labeling functions
+def distance(a,mid):
+    dist_vec=np.array((mid[0]-a[0],mid[1]-a[1]))
+    distance=np.linalg.norm(dist_vec)
+    return distance
+def get_atom_locs(_map):
+    atom_locs=[]
+    for h in _map:
+        corners=to_array(polygon_corners(layout,h))
+        for corner in corners:
+            atom_locs.append(corner)
+    return atom_locs
+def get_central_atom(atom_locs):
+    center_atom=[]
+    min_dist=image_size[0]
+    for a in atom_locs:
+        if distance(a,midpoint)<=min_dist:
+            min_dist=distance(a,midpoint)
+            center_atom=a
+    return center_atom
+
+def color_central_atom():
+    assert map != 'NoneType'
+    atom_locs=get_atom_locs(map)
+    central_atom=get_central_atom(atom_locs)
+    for i in range(3):
+        pixel_array[math.floor(central_atom[0])][math.floor(central_atom[1])][i]=255
+
+def color_neighbors():
+    assert map != 'NoneType'
+    atom_locs=get_atom_locs(map)
+    central_atom=get_central_atom(atom_locs)
+    neighbors=get_nearest_neighbors(atom_locs,central_atom)
+    i=0
+    for n in neighbors:
+        pixel_array[math.floor(n[0])][math.floor(n[1])][i]=225
+        pixel_array[math.floor(n[0])][math.floor(n[1])][i-1]=0
+        pixel_array[math.floor(n[0])][math.floor(n[1])][i-2]=0
+        i+=1
+    
+
+#Nearest neightbor vectors for "right positioned" atoms. Atoms on the left have negative vectors
+neighborvec1=np.array([0,0])
+neighborvec2=np.array([0,0])
+neighborvec3=np.array([0,0])
+def initialize_neighbor_vec():
+    global neighborvec1
+    global neighborvec2
+    global neighborvec3
+    neighborvec1=np.array([-size.x,0])
+    neighborvec2=np.array([math.sqrt(3)/2*size.x,size.y/2])
+    neighborvec3=np.array([math.sqrt(3)/2*size.x,-size.y/2])
+
+def get_nearest_neighbors(atom_locs,central):
+    neighbors=[]
+    isRight=False
+    left_neighbor_x=central[0]+neighborvec1[0]
+    for a in atom_locs:
+        if left_neighbor_x-2<=a[0] and left_neighbor_x+2>=a[0]:
+            isRight=True
+    if isRight:
+        neighbors.append(np.add(neighborvec1,central))
+        neighbors.append(np.add(neighborvec2,central))
+        neighbors.append(np.add(neighborvec3,central))
+    else:
+        neighbors.append(np.add(-neighborvec1,central))
+        neighbors.append(np.add(-neighborvec2,central))
+        neighbors.append(np.add(-neighborvec3,central))
+    return neighbors
+
+
 
 #%%
 #Drawing Hexogonal grid
 orange=(202,163,24)
-image_size=(512,512)
+image_size=[32,32]
 background_color=ImageColor.getrgb("hsl(46,0%,50%)")
 im=Image.new("RGB",image_size,color=background_color)
 pixel_array=np.array(im)
 blob_values_array=np.zeros((image_size[0],image_size[1]))
 d=ImageDraw.Draw(im)
-#origin=Point(image_size[0]//2,image_size[1]//2) #middle of the imgage
+midpoint=Point(image_size[0]//2,image_size[1]//2) #middle of the imgage
 origin=Point(0,0) #Top left corner
-size=Point(100,100)
-dot_size=math.floor(0.3*size[0])
-line_width=math.floor(.3*size[0])
+size=Point(8,8)
+dot_size=math.floor(0.3*min(size[0],size[1]))
+#line_width=math.floor(.3*size[0])
 structure_color=orange
 layout= Layout(layout_flat,size,origin)
-map=rect_map(4,4)
+#layout= Layout(layout_flat,size,midpoint)
+initialize_neighbor_vec()
+map=rect_map(3,4)
 
-#solid_circ(origin,300,"hsv(240,100%,50%)",d)
-#Gauss_circ_pixel((0,0),40)
+#gauss_circ_pixel((0,0),40)
+#plot_hex_dots(Hex(0,0,0),dot_size,structure_color,d,"Gaussian_pixel")
 for h in map:
     plot_hex_dots(h,dot_size,structure_color,d,"Gaussian_pixel")
-gauss_to_color()
-im=Image.fromarray(pixel_array)
 
+gauss_to_color()
+color_central_atom()
+color_neighbors()
+im=Image.fromarray(pixel_array)
 fig = plt.figure(figsize = (10, 10))
 ax = plt.subplot(1,1,1)
 ax.imshow(im)
 ax.axis('off')
-#im.save("Hex_lat_Gauss_blobs_biggeratoms.png")
-
+#im.save("Hex_lat_center_finder_5.png")
 
 #%% 
 im1=make_noisy_image(im,50)
