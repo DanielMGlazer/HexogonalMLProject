e746c9f49477ee7320468cd565696d14db1711ca
DanielMGlazer <66266159+DanielMGlazer@users.noreply.github.com>
2020-06-08 17:07:01 -0500

    Class confirmed to work properly

--- Hexagonal_Training_Data.py
+++ Hexagonal_Training_Data.py
@@ -60,40 +60,8 @@ def rect_map(map_height,map_width):
             map.append(Hex(q,r,-q-r))
     return map
 
-
-#%%
-#Function for drawing gaussian blobs
+#Gaussian function to be used globably
 Gaussian= lambda x,scl,amp: amp*math.e**(-0.5*(x/scl)**2)
-def gauss_circ(self,p,radius):
-    p[0]=int(p[0])
-    p[1]=int(p[1])
-    #radius is a given param that represents the standard deviation of the gaussian
-    #plot_rad is some multiple of the radius given, it is the radius of the circle to be plotted
-    _scale=radius
-    plot_rad=4*radius
-    color_amp=100
-    color_scaling=lambda x: Gaussian(x,_scale,color_amp)
-    
-    for x in range(max(p[0]-plot_rad,0),min(p[0]+plot_rad,image_size[0])):
-        y_max=math.floor(math.sqrt(plot_rad**2-(x-p[0])**2))
-        for y in range(max(p[1]-y_max,0),min(p[1]+y_max,image_size[1])):
-            dist_vec=np.array((x-p[0],y-p[1]))
-            distance=np.linalg.norm(dist_vec)
-            self.blob_values_array[y][x]+=color_scaling(distance)
-            
-def gauss_to_color(self):
-    for i in range(image_size[0]):
-        for j in range(image_size[1]):
-            rgbcolor=ImageColor.getrgb(f"hsl(46,{min(math.floor(self.blob_values_array[j][i]),100)}%,50%)")
-            self.pixel_array[j][i][0]=rgbcolor[0]
-            self.pixel_array[j][i][1]=rgbcolor[1]
-            self.pixel_array[j][i][2]=rgbcolor[2]
-
-def plot_hex_dots(self,h,radius):
-    corners=to_array(polygon_corners(layout,h))
-    #corners.append(hex_to_pixel(layout,h)) #Drawing center dot as well. Not needed 
-    for p in corners:
-        gauss_circ(self,p,radius)
 
 #%%
 #Labeling methods
@@ -101,72 +69,9 @@ def distance(a,mid): #Returns the distance between two pixels
     dist_vec=np.array((mid[0]-a[0],mid[1]-a[1]))
     distance=np.linalg.norm(dist_vec)
     return distance
-def get_atom_locs(_map): # Returns an array of all the atomic locations in the map
-    atom_locs=[]
-    for h in _map:
-        corners=to_array(polygon_corners(layout,h))
-        for corner in corners:
-            atom_locs.append(corner)
-    return atom_locs
-def get_central_atom(atom_locs): #Returns the atom closest to the center pixel of the image
-    center_atom=[]
-    min_dist=image_size[0]
-    for a in atom_locs:
-        if distance(a,midpoint)<=min_dist:
-            min_dist=distance(a,midpoint)
-            center_atom=a
-    return center_atom
 
-def color_central_atom(): #Draws the central atom in white in pixel_array
-    assert map != 'NoneType'
-    atom_locs=get_atom_locs(map)
-    central_atom=get_central_atom(atom_locs)
-    for i in range(3):
-        pixel_array[math.floor(central_atom[1])][math.floor(central_atom[0])][i]=255
 
-def color_neighbors(): #Draws the nearest neighbors in red, blue, and green in pixel_array
-    assert map != 'NoneType'
-    atom_locs=get_atom_locs(map)
-    central_atom=get_central_atom(atom_locs)
-    neighbors=get_nearest_neighbors(atom_locs,central_atom)
-    i=0
-    for n in neighbors:
-        pixel_array[math.floor(n[1])][math.floor(n[0])][i]=225
-        pixel_array[math.floor(n[1])][math.floor(n[0])][i-1]=0
-        pixel_array[math.floor(n[1])][math.floor(n[0])][i-2]=0
-        i+=1
-    
-#Nearest neightbor vectors for "right positioned" atoms. Atoms on the left have negative vectors
-neighborvec1=np.array([0,0])
-neighborvec2=np.array([0,0])
-neighborvec3=np.array([0,0])
-def initialize_neighbor_vec(): #Initializes the neighbor vectors based on size/bond lentgh
-    global neighborvec1
-    global neighborvec2
-    global neighborvec3
-    neighborvec1=np.array([-size[0],0])
-    neighborvec2=np.array([size[0]/2,math.sqrt(3)*size[1]/2])
-    neighborvec3=np.array([size[0]/2,-math.sqrt(3)*size[1]/2])
 
-def get_nearest_neighbors(atom_locs,central): #Returns the nearest neighbors to the central atom. 
-    #Doesn't search atom locations, just spits out where the neighbors should be given central location
-    neighbors=[]
-    isRight=False
-    left_neighbor_x=central[0]+neighborvec1[0]
-    for a in atom_locs:
-        if left_neighbor_x-2<=a[0] and left_neighbor_x+2>=a[0]:
-            isRight=True
-    if isRight:
-        neighbors.append(np.add(neighborvec1,central))
-        neighbors.append(np.add(neighborvec2,central))
-        neighbors.append(np.add(neighborvec3,central))
-        print("Right")
-    else:
-        neighbors.append(np.add(-neighborvec1,central))
-        neighbors.append(np.add(-neighborvec2,central))
-        neighbors.append(np.add(-neighborvec3,central))
-        print("Left")
-    return neighbors
     
 #%%
 #Class for training images
@@ -179,11 +84,12 @@ class Training_im:
         self.bl=bond_lengths
         self.o=origin
         self.atmsz=atom_size
-        self.im=Image.new("RGB",image_size,color=background_color)
+        self.im=Image.new("RGB",self.image_size,color=self.background_color)
         self.pixel_array=np.array(self.im)
         self.blob_values_array=np.zeros((self.image_size[1],self.image_size[0]))
         self.mid=Point(self.image_size[0]//2,self.image_size[1]//2)
         self.layout=Layout(layout_flat,self.bl,self.o)
+        self.make_im()
     #Plotting methods
     def gauss_circ(self,p,radius):
         p[0]=int(p[0])
@@ -195,16 +101,16 @@ class Training_im:
         color_amp=100
         color_scaling=lambda x: Gaussian(x,_scale,color_amp)
         
-        for x in range(max(p[0]-plot_rad,0),min(p[0]+plot_rad,image_size[0])):
+        for x in range(max(p[0]-plot_rad,0),min(p[0]+plot_rad,self.image_size[0])):
             y_max=math.floor(math.sqrt(plot_rad**2-(x-p[0])**2))
-            for y in range(max(p[1]-y_max,0),min(p[1]+y_max,image_size[1])):
+            for y in range(max(p[1]-y_max,0),min(p[1]+y_max,self.image_size[1])):
                 dist_vec=np.array((x-p[0],y-p[1]))
                 distance=np.linalg.norm(dist_vec)
                 self.blob_values_array[y][x]+=color_scaling(distance)
             
     def gauss_to_color(self):
-        for i in range(image_size[0]):
-            for j in range(image_size[1]):
+        for i in range(self.image_size[0]):
+            for j in range(self.image_size[1]):
                 rgbcolor=ImageColor.getrgb(f"hsl(46,{min(math.floor(self.blob_values_array[j][i]),100)}%,50%)")
                 self.pixel_array[j][i][0]=rgbcolor[0]
                 self.pixel_array[j][i][1]=rgbcolor[1]
@@ -217,26 +123,32 @@ class Training_im:
             self.gauss_circ(p,radius)
 
     #Neighbor methods
-
+    def get_atom_locs(self,_map): # Returns an array of all the atomic locations in the map
+        atom_locs=[]
+        for h in _map:
+            corners=to_array(polygon_corners(self.layout,h))
+            for corner in corners:
+                atom_locs.append(corner)
+        return atom_locs
     def get_central_atom(self,atom_locs): #Returns the atom closest to the center pixel of the image
         center_atom=[]
         min_dist=self.image_size[0]
         for a in atom_locs:
             if distance(a,self.mid)<=min_dist:
-                min_dist=distance(a,midpoint)
+                min_dist=distance(a,self.mid)
                 center_atom=a
         return center_atom
 
     def color_central_atom(self): #Draws the central atom in white in pixel_array
         assert self.map != 'NoneType'
-        atom_locs=get_atom_locs(self.map)
+        atom_locs=self.get_atom_locs(self.map)
         central_atom=self.get_central_atom(atom_locs)
         for i in range(3):
             self.pixel_array[math.floor(central_atom[1])][math.floor(central_atom[0])][i]=255
 
     def color_neighbors(self): #Draws the nearest neighbors in red, blue, and green in pixel_array
         assert self.map != 'NoneType'
-        atom_locs=get_atom_locs(self.map)
+        atom_locs=self.get_atom_locs(self.map)
         central_atom=self.get_central_atom(atom_locs)
         neighbors=self.get_nearest_neighbors(atom_locs,central_atom)
         i=0
@@ -258,40 +170,46 @@ class Training_im:
         isRight=False
         left_neighbor_x=central[0]+self.neighborvec1[0]
         for a in atom_locs:
-            if left_neighbor_x-2<=a[0] and left_neighbor_x+2>=a[0]:
+            if left_neighbor_x-1<=a[0] and left_neighbor_x+1>=a[0]:
                 isRight=True
         if isRight:
             neighbors.append(np.add(self.neighborvec1,central))
             neighbors.append(np.add(self.neighborvec2,central))
             neighbors.append(np.add(self.neighborvec3,central))
-            print("Right")
         else:
             neighbors.append(np.add(-self.neighborvec1,central))
             neighbors.append(np.add(-self.neighborvec2,central))
             neighbors.append(np.add(-self.neighborvec3,central))
-            print("Left")
         return neighbors
     
     def make_im(self):
         
         self.initialize_neighbor_vec()
-        self.map=rect_map(3,4)
-
+        mapsize_x=math.floor(self.image_size[0]/(math.sqrt(3)*self.bl[0])*2)
+        mapsize_y=math.floor(self.image_size[1]/(2*self.bl[1])*2)
+        self.map=rect_map(mapsize_x,mapsize_y)
         for h in self.map:
-            self.plot_hex_dots(h,dot_size)
-        
+            self.plot_hex_dots(h,self.atmsz)
         self.gauss_to_color()
+        self.im_fin=Image.fromarray(self.pixel_array)
+    def color_data(self):
         self.color_central_atom()
         self.color_neighbors()
         self.im_fin=Image.fromarray(self.pixel_array)
 
 #%%
 #Plotting images
-im=Training_im([8,8],[0,0],2)
-im.make_im()
+bond_lengths=[8,8]
+origin=[-4,0]
+atom_size=2
+im=Training_im(bond_lengths,origin,atom_size)
+#im.color_data()
 fig = plt.figure(figsize = (10, 10))
 ax = plt.subplot(1,1,1)
 ax.imshow(im.im_fin)
 ax.axis('off')
 
 
+
+
+#%%
